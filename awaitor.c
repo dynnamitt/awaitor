83996fb711f5376e6e9da2d0f303f7ad16acf8c2
Kjetil Flovild-Midtlie <kfm@docstream.no>
2015-03-28 18:06:38 +0100

    flush-off,listener can pipe into shell

--- awaitor.c
+++ awaitor.c
@@ -13,16 +13,24 @@ event_handler(struct mg_connection *conn,
     puts("Bingo");
     return MG_TRUE;   // Mark as processed
   } else {
-    printf("skippin this ev: %d\n",ev);
+    printf("skippin this ev: %d , uri:%s\n",ev,conn->uri);
     return MG_FALSE;  // Rest of the events are not processed
   }
 }
 
 int
 main(void) {
+
+  char *port = "8080";
+
   struct mg_server *server = mg_create_server(NULL, event_handler);
   mg_set_option(server, "document_root", ".");
-  mg_set_option(server, "listening_port", "8080");
+  mg_set_option(server, "listening_port", port );
+
+  printf("server up on port %s.\n",port);
+
+  setbuf(stdout, NULL);
+  
 
   for (;;) {
     mg_poll_server(server, 1000);  // Infinite loop, Ctrl-C to stop
