17140444f20537801553d9eca35eae26a3afb95d
Kjetil Flovild-Midtlie <kfm@docstream.no>
2015-03-29 13:29:11 +0200

    soon-a-prog

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = c99
-CFLAGS = -g -Wall -O3
+CFLAGS = -g -Wall -O3 
 CPPFLAGS = -Imongoose -Ijsmn
 LDLIBS = -lpthread
 
@@ -8,7 +8,7 @@ awaitor: jsmn/libjsmn.a mongoose/mongoose.o
 
 clean:
 	cd jsmn && $(MAKE) clean
-	rm -rf awaitor mongoose.o .jsmn.hg
+	rm -rf awaitor mongoose/mongoose.o .jsmn.hg
 
 # -----------
 #  DEPS
@@ -16,6 +16,8 @@ clean:
 
 jsmn/%.a: jsmn
 	cd jsmn && $(MAKE)
+
+mongoose/%.o: CPPFLAGS += -DMONGOOSE_NO_THREADS -DMONGOOSE_NO_FILESYSTEM -DMONGOOSE_NO_CGI
 
 jsmn: JSMN_REPO = http://bitbucket.org/zserge/jsmn
 jsmn: JSMN_COMMIT = 6979f2e
--- awaitor.c
+++ awaitor.c
@@ -1,36 +1,71 @@
 #include "mongoose.h"
 #include "jsmn.h"
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+// CONFIG 
+typedef struct {
+  int argc;
+  char **argv;
+  char *port_no;
+  char *ips_allowed;
+} config_t;
 
 static int
 event_handler(struct mg_connection *conn,
-              enum mg_event ev) {
+              enum mg_event ev)
+{
+  printf(" ---> threadid %d :" , (int)pthread_self());
   if (ev == MG_AUTH) {
+    printf("In AUTH %s\n",conn->uri);
     return MG_TRUE;   // Authorize all requests
   } else if (ev == MG_REQUEST && !strcmp(conn->uri, "/hello")) {
     //
     mg_printf_data(conn, "%s", "Hello world");
-    puts("Bingo");
+    char * cmd = null;
+    for (int i=0 ; i< conn->server_param->argc; i++) {
+      
+    }
+    printf("time to EXEC %s",cmd);
+    puts("++++++++++++++");
     return MG_TRUE;   // Mark as processed
   } else {
-    printf("skippin this ev: %d , uri:%s\n",ev,conn->uri);
+    printf("NOOP. ev: %d , uri:%s\n",ev,conn->uri);
     return MG_FALSE;  // Rest of the events are not processed
   }
 }
 
 int
-main(void) {
+main(int argc, char *argv[]) {
 
-  char *port = "8080";
+  // grab CONFIG from args + env
+  config_t config = {
+    .argc = argc,
+    .argv = argv,
+    .port_no = getenv("PORT"),
+    .ips_allowed = getenv("IPS_ALLOWED"),
+  };
 
-  struct mg_server *server = mg_create_server(NULL, event_handler);
-  mg_set_option(server, "document_root", ".");
-  mg_set_option(server, "listening_port", port );
+  // Set default CONF values if missing
+  if (!config.port_no) { config.port_no = "8080"; }
+  if (!config.ips_allowed) {
+    config.ips_allowed = "127.0.0.1,";
+  }
 
-  printf("server up on port %s.\n",port);
+  // init www server
+  struct mg_server *server = mg_create_server(&config, event_handler);
+  mg_set_option(server, "listening_port", config.port_no );
 
+  printf("server up on port %s, ", config.port_no );
+  printf("threadid %d .\n" , (int)pthread_self());
+
+  // no-buf-flush. To notify UNIX pipes asap
   setbuf(stdout, NULL);
-  
 
   for (;;) {
     mg_poll_server(server, 1000);  // Infinite loop, Ctrl-C to stop
