50d227f11502565a5d572ea58b059c3d0f1c5072
Kjetil Flovild-Midtlie <kjetil@flovild-midtlie.name>
2015-03-31 10:40:52 +0200

    Update dm_str.c

--- dm_str.c
+++ dm_str.c
@@ -3,6 +3,7 @@
 
 int starts_with( const char *str, const char *pre )
 {
+    // TODO optimize w while-loop; DONT use strlen
     size_t lenpre = strlen(pre),
            lenstr = strlen(str);
     return lenstr < lenpre ? 0 : strncmp(pre, str, lenpre) == 0;
