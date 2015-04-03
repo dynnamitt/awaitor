45fcbdcf45f05edb5655303ffba5dd5f47ac02c0
Kjetil Flovild-Midtlie <kfm@docstream.no>
2015-04-03 20:12:47 +0200

    cleanup deluxe

--- awaitor.c
+++ awaitor.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <unistd.h>
-#include <pthread.h>
+#include <stdbool.h>
 
 #include "dbg.h"
 #include "mongoose.h"
 #include "jsmn.h"
 
+/// globals
+#define SERVER_ERROR 500
+#define DEF_PORT "8080"
+#define OPT_ACL "access_control_list"
+#define OPT_PORT "listening_port"
 
-// CONFIG
-typedef struct {
-  char **argv;
-  char *port_no;
-  char *ips_allowed;
-} config_t;
+char **ext_cmd;
 
-config_t config;
-
-void
+/// usage msg on error
+static void
 usage(char *arg0)
 {
   puts("Usage:");
@@ -26,18 +26,36 @@ usage(char *arg0)
   return;
 }
 
-
-int
-starts_with(const char *restrict string,
-            const char *restrict prefix)
+/// Checking if string in arg0 starts w prefix, arg1.
+static bool
+starts_with(const char *string,
+            const char *prefix)
 {
   while(*prefix){
     if(*prefix++ != *string++)
-      return 0;
+      return false;
   }
-  return 1;
+  return true;
 }
 
+/// exec-and-fork.
+static bool
+exec(void)
+{
+     printf("    ");
+
+    char **cmd = ext_cmd;
+    while(*cmd){
+      printf("%s ",*cmd);
+      cmd++;
+    }
+
+    printf("\n");
+
+    return true;
+}
+
+/// the mongoose EV handler, THE SERVER basically.
 static int
 event_handler(struct mg_connection *conn,
               enum mg_event ev)
@@ -49,23 +67,20 @@ event_handler(struct mg_connection *conn,
   } else if (ev == MG_REQUEST &&
              starts_with(conn->uri, "/hello_")) {
 
-    printf("    ");
+    bool s_code = exec();
 
-    char **cmd = config.argv;
-    while(*cmd){
-      printf("%s ",*cmd);
-      cmd++;
+    if(s_code){
+      mg_printf_data(conn, "%s", "'OK'");
+    } else {
+      mg_send_status(conn, SERVER_ERROR);
+      mg_printf_data(conn, "%s", "'Failed to exec predefined CMD'");
     }
-
-    printf("\n");
-
-    mg_printf_data(conn, "%s", "Hello world");
     return MG_TRUE;   // Mark as processed
-
   } else {
     return MG_FALSE;  // Rest of the events are not processed
   }
 }
+
 
 int
 main(int argc, char *argv[])
@@ -76,31 +91,28 @@ main(int argc, char *argv[])
     return 1;
   }
 
-  // grab CONFIG from args + env
-  config.argv = calloc(argc,sizeof(char *));
-  check_mem(config.argv);
-  config.argv = &argv[1];
-  config.port_no = getenv("PORT");
-  config.ips_allowed = getenv("IPS_ALLOWED");
+  ext_cmd = calloc(argc, sizeof(char *));
+  check_mem(ext_cmd);
+  ext_cmd = &argv[1];
 
-  // Set default `config values if missing
-  if (!config.port_no) { config.port_no = "8080"; }
-  if (!config.ips_allowed) {
-    config.ips_allowed = "127.0.0.1,";
-  }
+  char *port_no = getenv("PORT");
+  if (!port_no) port_no = DEF_PORT;
+
+  char *acl = getenv("ACL");
+  if (!acl) acl = "";
 
   // init www server
   struct mg_server *server = mg_create_server(NULL, event_handler);
   check_mem(server);
-  const char *err = mg_set_option(server, "listening_port", config.port_no );
-  check(!err,"Cannot start www server.\n%s",err);
-  /* mg_set_option(server, "access_log_file", "/dev/stdout"); */
+  const char *pn_err = mg_set_option(server, OPT_PORT, port_no );
+  check(!pn_err,"Cannot start www server.\n%s",pn_err);
+  const char *acl_err = mg_set_option(server, OPT_ACL, acl);
+  check(!acl_err,"Cannot init ACL. %s",acl_err);
 
+  printf("Awaiting... on %s\n",port_no);
 
-  printf("Awaiting... on %s\n",config.port_no);
-
-  // no-buf-flush. To notify UNIX pipes asap
-  setbuf(stdout, NULL);
+  // no-buf n flush. To notify UNIX pipes asap
+  // setbuf(stdout, NULL);
 
   for (;;) {
     mg_poll_server(server, 1000);  // Infinite loop, Ctrl-C to stop
